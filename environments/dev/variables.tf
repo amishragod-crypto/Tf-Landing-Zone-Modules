301249da74d9ad3623acc9cbb787967f4d34bbaf
Atul Kumar Mishra <amishra.god@gmail.com>
2026-08-01 13:43:32 +0530

    new Commit

---
+++ environments/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_groups" {}
+
+variable "storage_accounts" {}
+
+variable "virtual_networks" {}
+
+variable "subnets" {}
+
+variable "public_ips" {}
+
+variable "linux_virtual_machines" {}
