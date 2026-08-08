22eb0db0670c826d780a8f03e2bf1f78de764db0
Atul Kumar Mishra <amishra.god@gmail.com>
2026-08-08 21:17:05 +0530

    rename some files

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,34 +1,34 @@
 module "resource_group" {
-  source          = "../../1_azurerm_resource_group"
+  source          = "../../Child_Modules/1_azurerm_resource_group"
   resource_groups = var.resource_groups
 }
 
 module "storage_account" {
   depends_on       = [module.resource_group]
-  source           = "../../2_azurerm_storage_account"
+  source           = "../../Child_Modules/2_azurerm_storage_account"
   storage_accounts = var.storage_accounts
 }
 
 module "virtual_network" {
   depends_on       = [module.resource_group]
-  source           = "../../3_azurerm_virtual_network"
+  source           = "../../Child_Modules/3_azurerm_virtual_network"
   virtual_networks = var.virtual_networks
 }
 
 module "subnet" {
   depends_on = [module.virtual_network]
-  source     = "../../4_azurerm_subnet"
+  source     = "../../Child_Modules/4_azurerm_subnet"
   subnets    = var.subnets
 }
 
 module "public_ip" {
     depends_on = [module.resource_group]
-  source = "../../5_azurerm_public_ip"
+  source = "../../Child_Modules/5_azurerm_public_ip"
   public_ips = var.public_ips
 }
 
 module "linux_virtual_machine" {
   depends_on       = [module.resource_group, module.subnet, module.public_ip]
-  source           = "../../6_azurerm_linux_virtual_machine"
+  source           = "../../Child_Modules/6_azurerm_linux_virtual_machine"
   linux_virtual_machines = var.linux_virtual_machines
 }
