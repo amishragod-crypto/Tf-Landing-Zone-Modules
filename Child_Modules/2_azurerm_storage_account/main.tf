ecde85a146adf08389e89b54359c3c672c22a6ae
Atul Kumar Mishra <amishra.god@gmail.com>
2026-08-09 12:41:50 +0530

    Terraform pipeline trigger

--- Child_Modules/2_azurerm_storage_account/main.tf
+++ Child_Modules/2_azurerm_storage_account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "Saccounts" {
-  for_each = var.storage_accounts
+  for_each                 = var.storage_accounts
   name                     = each.value.name
   resource_group_name      = each.value.resource_group_name
   location                 = each.value.location
