a026f5db6db9fb7fdf2cdade35461dfb6f338090
Atul Kumar Mishra <amishra.god@gmail.com>
2026-08-12 19:40:45 +0530

    tfsec issues resolved v2

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,11 +1,56 @@
-variable "resource_groups" {}
+variable "resource_groups" {
+    type = map(object({
+        name     = string
+        location = string
+    }))
+}
 
-variable "storage_accounts" {}
+variable "storage_accounts" {
+    type = map(object({
+        name                     = string
+        resource_group_name      = string
+        location                 = string
+        account_tier             = string
+        account_replication_type = string
+    }))
+}
 
-variable "virtual_networks" {}
+variable "virtual_networks" {
+    type = map(object({
+        name                = string
+        resource_group_name = string
+        location            = string
+        address_space       = list(string)
+    }))
+}
 
-variable "subnets" {}
+variable "subnets" {
+    type = map(object({
+        name                 = string
+        resource_group_name  = string
+        virtual_network_name = string
+        address_prefixes     = list(string)
+    }))
+}
 
-variable "public_ips" {}
+variable "public_ips" {
+    type = map(object({
+        name                 = string
+        resource_group_name  = string
+        location             = string
+        allocation_method    = string
+        sku                  = string
+    }))
+}
 
-variable "linux_virtual_machines" {}
+variable "linux_virtual_machines" {
+    type = map(object({
+        name                 = string
+        resource_group_name  = string
+        location             = string
+        size                 = string
+        admin_username       = string
+        admin_password       = string
+        network_interface_id = string
+    }))
+}
